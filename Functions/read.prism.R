0b02cde3d6ee0d015591c04787d5985a5fc9e262
Amy Hessl <amy.hessl@mail.wvu.edu>
2019-02-11 14:48:46 -0500

    Added daily and monthly data folders of multiple grids; updated read.prism.R for the folders; commented out broken parts of PrismSynth.Rmd

--- Functions/read.prism.R
+++ Functions/read.prism.R
@@ -1,12 +1,13 @@
 #read.prism.R
 #Function to read PRISM time series data
+#add argument for assigning the directory
 
-prism.files <- list.files("data/") ##how can I assign names from filenames?
+prism.files <- list.files("data/daily_cv") ##how can I assign names from filenames?
 grid.names <- sub('.*201*0110_', '', prism.files)
 
      
-dataFiles <- lapply(Sys.glob("data/*.csv"), read.csv, skip=11, head=F)
-lapply(Sys.glob("data/*.csv"), read.csv, skip=11, head=F)
+dataFiles <- lapply(Sys.glob("data/daily_cv/*.csv"), read.csv, skip=11, head=F)
+lapply(Sys.glob("data/daily_cv/*.csv"), read.csv, skip=11, head=F)
 
 climvars <- c("pdate", "ppt", "tmin", "tmean", "tmax")
 dataFiles <- lapply(dataFiles, setNames, climvars)
