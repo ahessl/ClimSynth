936502f78d57a0a12661407be1b2cce92ddac613
Amy Hessl <amy.hessl@mail.wvu.edu>
2019-05-11 11:30:03 -0400

    revised read.prism.R function, no longer calculates mean of the files, simply returns a list of the files.

--- Functions/read.prism.R
+++ Functions/read.prism.R
@@ -1,33 +1,18 @@
 #read.prism.R
-#Function to read PRISM time series data
-#add argument for assigning the directory
-read.prism <- function(data.dir) {
-    data.path <- paste0("Data/", data.dir)
+#Function to read PRISM time series data from a directory
+#Rename the col headings, return a list of all prism files in directory
+
+read.prism <- function(site) {
+    data.path <- "data/climate_data"
     
-    prism.files <- list.files(data.path) ##how can I assign names from filenames?
-    grid.names <- sub('.*201*07_', '', prism.files)
-    
+    prism.files <- list.files(data.path) 
+
     glob.path <- paste0(data.path, "/*.csv")   
     
-    #dataFiles <- lapply(Sys.glob("data/daily_cv/*.csv"), read.csv, skip=11, head=F)
     dataFiles <- lapply(Sys.glob(glob.path), read.csv, skip=11, head=F)
     
     
     climvars <- c("pdate", "ppt", "tmin", "tmean", "tmax")
     dataFiles <- lapply(dataFiles, setNames, climvars)
-    
-    #Create a mean of the dataFrames list
-    #abind combines dataframes (or matrices, vectors etc) into arrays along a dimension
-    #first remove the date column and save it
-    dataFiles.num <- lapply(dataFiles, function(x) { x["pdate"] <- NULL; x })
-    pdate <- dataFiles[[1]]$pdate ###Problem for monthly data
-    
-    dataFiles.mn <- apply(abind::abind(dataFiles.num, along = 3), 1:2, mean, na.rm=T) #returns matrix 
-    
-    pgrid.df <- data.frame(dataFiles.mn)
-    pgrid.df$pdate <- pdate
-    
-    pgrid.output <- list (prism.files=prism.files, grid.names=grid.names, dataFiles=dataFiles, pgrid.df=pgrid.df)
-
 }
 
