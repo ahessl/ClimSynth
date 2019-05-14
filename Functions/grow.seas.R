3eefde95db93cb5d8ae73580b778356f838595c7
Amy Hessl <amy.hessl@mail.wvu.edu>
2019-05-14 09:46:38 -0400

    Creates means by selected months.  Function nearly complete. Return df still messy.

--- Functions/grow.seas.R
+++ Functions/grow.seas.R
@@ -3,33 +3,39 @@
 
 library(tidyverse)
 library(lubridate)
-library(seas)
 
 source("Functions/read.prism.R")
 
 pgrid.list <- read.prism("mon_cv")
 pgrid <- pgrid.list[[1]]
 
-# Mean by seasons
-# create seasons using lubridate::quarter
-pgrid$pdate <- paste0(pgrid$pdate, "-01")
-pgrid$quarters <- quarter(pgrid$pdate, fiscal_start=12, with_year=TRUE)
 
-# create mean of seasons using aggregate
-seas_mns <- aggregate(pgrid[,c(3:5)], by=list(pgrid$quarters), mean)
+# grow.seas using long form prism climate data, select months to summarize, starting month for 
+# growing season year, TBD select mean or total
+grow.seas <- function (pgrid, sel_mons = c(5:7), st_mon = 11){
 
-# Mean by selected months
-pgrid <- pgrid.list[[1]]
-sel_mons <- c(5,6,7) #months to select
+    sel_mons <- c(5:7) #months to select
+    st_mon <- 11 #start month for grow year (rel for NH winter)
+    
+    sel_mon_l <- paste0(substr(month.abb[sel_mons], 1, 1), collapse='') #first letter of selected months
+    
+    
+    pgrid$pdate <- paste0(pgrid$pdate, "-01")
+    pgrid$mon <- month(pgrid$pdate)
+    pgrid$year <- year(pgrid$pdate)
+    
+    #Add if statement for adjusting winter months to next season
+    pgrid$adj_yr <- ifelse(pgrid$mon>=st_mon, 
+                           pgrid$year+1, 
+                           pgrid$year)
 
-pgrid$pdate <- paste0(pgrid$pdate, "-01")
-pgrid$mon <- month(pgrid$pdate)
-pgrid$year <- year(pgrid$pdate)
 
-#Add if statement for adjusting winter months to next season
-#if(pgrid$mon>10) pgrid$year+1 else pgrid$year
+    pgrid$gr_seas <- ifelse(pgrid$mon %in% sel_mons,
+                            sel_mon_l, 
+                            "NA")
 
-pgrid$sel <- pgrid$mon %in% sel_mons
+    gr_seas_df <- aggregate(pgrid[,c(3:5)], by=list(pgrid$gr_seas, pgrid$year), mean)
+    gr_seas_df <- gr_seas_df[gr_seas_df$Group.1==sel_mon_l,]
+    colnames(gr_seas_df[1:2]) <- c("gr_seas", "year")
 
-x <- aggregate(pgrid[,c(3:5)], by=list(pgrid$sel, pgrid$year), mean)
-x[x$Group.1=="TRUE",]
+    return(gr_seas_df)
