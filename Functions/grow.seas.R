6e6c0e40f36c9701d49f3f1ee048e4d0d7b76865
Amy Hessl <amy.hessl@mail.wvu.edu>
2019-05-14 17:16:56 -0400

    Adding some warnings to grow.seas.R

--- Functions/grow.seas.R
+++ Functions/grow.seas.R
@@ -1,26 +1,24 @@
 # grow.seas.R
-# calculate a growing season mean of long form climate data
+# calculate a growing season mean of long form prism monthly data
+# arguments include pgrid (longform monthly climate data)
+# months to select for growing season 1:12 (sel_mons) , 
+# variables to select from the climate data (sel_vars),
+# month to start the growth year (st_mon) - only relevant
+# if including winter months, and summary method mean or sum
+# (sum_f)
+
 # grow.seas (pgrid, sel_mons = c(5:7), sel_vars = c(3:5), 
 # st_mon = 11, sum_f = c(mean, sum))
 
-sel_mons = c(5:7)
-sel_vars = c(2)
-st_mon = 11
-sum_f = sum
-
-
 library(lubridate)
 
-source("Functions/read.prism.R")
-
-pgrid.list <- read.prism("mon_cv")
-pgrid <- pgrid.list[[1]]
-
-
-# grow.seas using long form prism climate data (pgrid), select months to summarize (), starting month for 
 # growing season year (st_mon), summary function (sum_f)
 grow.seas <- function (pgrid, sel_mons, sel_vars, st_mon, 
                        sum_f){
+    
+    stopifnot(is.numeric(sel_mons), is.numeric(sel_vars),
+              is.numeric(st_mon), max(sel_mons)<=12, st_mon>12, 
+              is.character(sum_f))
   
     sel_mon_l <- paste0(substr(month.abb[sel_mons], 1, 1), collapse='') #first letter of selected months
     var_names <- colnames(pgrid[sel_vars])
@@ -45,5 +43,6 @@ grow.seas <- function (pgrid, sel_mons, sel_vars, st_mon,
     return(gr_seas_df)
 }
 
-x <- grow.seas (pgrid, sel_mons = c(5:7), sel_vars = c(3:5), 
-    st_mon = 11, sum_f = mean)
+
+
+
